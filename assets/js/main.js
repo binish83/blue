35ee00412983d6bad2793882d7bc2fdef62b4505
Binish Ali <binish83@gmail.com>
2019-12-01 17:15:37 -0500

    Disabled JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-$(".jumbotron").css({ height: $(window).height() + "px" });
-
-$(window).on("resize", function() {
-	$(".jumbotron").css({ height: $(window).height() + "px" });
-});
+// $(".jumbotron").css({ height: $(window).height() + "px" });
+//
+// $(window).on("resize", function() {
+// 	$(".jumbotron").css({ height: $(window).height() + "px" });
+// });
